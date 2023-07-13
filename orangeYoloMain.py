8371939ab91bf3f6d84361628c114a96963983fc
Ocetars <ocetars@gmail.com>
2023-07-13 14:06:04 +0800

    draw迁移至主函数

--- orangeYoloMain.py
+++ orangeYoloMain.py
@@ -4,6 +4,7 @@ from rknnpool import rknnPoolExecutor
 from func import myFunc
 
 cap = cv2.VideoCapture("./VID_20230712_172958.mp4")
+# cap = cv2.VideoCapture("./REC_for_testing.mp4")
 # cap = cv2.VideoCapture(0)
 
 # 模型路径
@@ -25,7 +26,47 @@ if cap.isOpened():
             exit(-1)
         pool.put(frame)
 
+def draw(image, boxes, scores, classes, label="A", threshold=0.6):
+    centers = []
+    for box, score, cl in zip(boxes, scores, classes):
+        # 加入筛选条件，确定需要检测的目标
+        if CLASSES[cl] == label and score > threshold:
+            top, left, right, bottom = box
+            # print('class: {}, score: {}'.format(CLASSES[cl], score))
+            # print('box coordinate left,top,right,down: [{}, {}, {}, {}]'.format(top, left, right, bottom))
+            top = int(top)  # 左上x1
+            left = int(left)  # 左上y1
+            right = int(right)  # 右下x2
+            bottom = int(bottom)  # 右下y2
+            x1 = top
+            y1 = left
+            x2 = right
+            y2 = bottom
+            # 在图像上绘制目标框
+            cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+            # 计算目标框的中心坐标
+            center_x = (x1 + x2) / 2
+            center_y = (y1 + y2) / 2
+            centers.append((center_x, center_y))
+
+            # 在图像上绘制目标中心
+            cv2.circle(image, (int(center_x), int(center_y)), 2, (0, 255, 0), -1)
+            # 在图像上绘制物体类别及置信度
+            cv2.putText(
+                image,
+                "{0} {1:.2f}".format(CLASSES[cl], score),
+                (x1, y1 - 6),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.6,
+                (0, 0, 255),
+                2,
+            )
+    return centers, score
+
 frames, loopTime, initTime = 0, time.time(), time.time()
+
+# 主函数
 while cap.isOpened():
     frames += 1
     ret, frame = cap.read()
@@ -39,21 +80,19 @@ while cap.isOpened():
     # 输出结果，result是列表，内容详见myFunc函数
     if result is not None:
         outpic = result[0]
-        centers = result[1]
-        boxes = result[2]
-        scores = result[3]
-        classes = result[4]
-        
-        cv2.imshow("outpic", outpic)
+        boxes = result[1]
+        scores = result[2]
+        classes = result[3]
+    
         if classes is not None:
-            for cl in classes:
-                # if CLASSES[cl] == "W":
-                if centers:
-                    # centers是一个列表，里面第[0]个元素是一个元组，元组中是中心点的坐标
-                    center_x = centers[0][0]
-                    center_y = centers[0][1]
-                    print("center:\t",center_x,center_y)
-                    print("class:\t", CLASSES[cl])
+            # 从draw函数中引入返回值
+            centers, score = draw(outpic, boxes, scores, classes)
+
+            print(  "class:\t", 'A',
+                    "\tcenters:\t", centers, 
+                    "\tscore:\t", score  )
+            
+            cv2.imshow("outpic", outpic)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
     # if frames % 30 == 0:
